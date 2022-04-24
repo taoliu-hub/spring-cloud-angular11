b8c378cd40cf1891873b18f0d16059a6fb6d6f81
liutao <liu123tao321@163.com>
2022-04-24 14:00:28 +0800

    commit init config server

---
+++ spring-cloud/config-server/src/test/java/cn/peach/ConfigServerApplicationTest.java
@@ -0,0 +1,20 @@
+package cn.peach;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ConfigServerApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
--- spring-cloud/product-view-service-feign/src/main/java/cn/peach/controller/ProductController.java
+++ spring-cloud/product-view-service-feign/src/main/java/cn/peach/controller/ProductController.java
@@ -3,6 +3,8 @@ package cn.peach.controller;
 import cn.peach.pojo.Product;
 import cn.peach.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +12,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class ProductController {
+
+    @Value("${version}")
+    String version;
 
     @Autowired
     ProductService productService;
@@ -19,6 +25,7 @@ public class ProductController {
     public Object products(Model m) {
         List<Product> ps = productService.listProducts();
         m.addAttribute("ps", ps);
+        m.addAttribute("version", version);
         return "products";
     }
 }
