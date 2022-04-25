d9ba83816c03a2fb830e530c3c16137b27872406
Liu Tao <tao.liu1@ncsi.com.cn>
2022-04-25 12:10:10 +0800

    commit add "断路器监控，断路器聚合监控，网关Zuul"

---
+++ spring-cloud/zuul/src/test/java/cn/peach/ProductServiceZuulApplicationTest.java
@@ -0,0 +1,17 @@
+package cn.peach;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class ProductServiceZuulApplicationTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
